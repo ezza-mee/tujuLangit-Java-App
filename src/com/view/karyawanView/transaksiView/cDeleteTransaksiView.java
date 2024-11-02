9a425045ceab0ee1aa56396dc08cc69688149af2
ezza-mee <rezaaryairhamilah@gmail.com>
2024-11-02 23:30:47 +0700

    add-component-input-update-delete-transaksi

--- src/com/view/karyawanView/transaksiView/cDeleteTransaksiView.java
+++ src/com/view/karyawanView/transaksiView/cDeleteTransaksiView.java
@@ -1,7 +1,62 @@
 package com.view.karyawanView.transaksiView;
 
-import com.main.resources.templates.cPanelContentApp;
+import javax.swing.JLabel;
 
-public class cDeleteTransaksiView extends cPanelContentApp{
-    
+import com.main.resources.templates.cPanelContentApp;
+import com.partials.*;
+import com.view.karyawanView.cPanelContentView;
+
+public class cDeleteTransaksiView extends cPanelContentApp {
+
+    // add object cPanelContentView
+    private cPanelContentView parentPanel;
+
+    // add label header Transaksi
+    private cBigFont labelHeaderTransaksi = new cBigFont("Transaksi", 40, 5);
+    private cBigFont labelDeleteTransaksi = new cBigFont("Delete Data Transaksi", 40, 20);
+
+    // component copyright
+    private cLabelInfo labelCopyright = new cLabelInfo("CopyRight 2024. TujuLangit ForestPark", 0, 650, 1126, 40);
+
+    // component panel Data transaksi
+    private cPanelRounded panelDataTransaksi = new cPanelRounded(40, 80, 1050, 560, 10, 10);
+
+    // component button delete Transaksi
+    private cButtonRounded btnDeleteTransaksi = new cButtonRounded("Delete", 660, 460, 110, 40, 10);
+    private cButtonRounded btnBackToHome = new cButtonRounded("Back", 780, 460, 110, 40, 10);
+
+    public cDeleteTransaksiView(cPanelContentView parentPanel) {
+        super();
+        this.parentPanel = parentPanel;
+
+        initsDeleteTransaksiView();
+    }
+
+    private void initsDeleteTransaksiView() {
+        setVisible(true);
+
+        btnBackToHome.addActionListener(new java.awt.event.ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent ae) {
+                parentPanel.showDataTransaksiView();
+            }
+        });
+
+        btnDeleteTransaksi.setFont(cFonts.BUTTON_INPUT_FONT);
+        btnBackToHome.setFont(cFonts.BUTTON_INPUT_FONT);
+
+        labelCopyright.setHorizontalAlignment(JLabel.CENTER);
+        labelCopyright.setFont(cFonts.COPYRIGHT_FONT);
+
+        panelDataTransaksi.add(labelDeleteTransaksi);
+
+        panelDataTransaksi.add(btnDeleteTransaksi);
+        panelDataTransaksi.add(btnBackToHome);
+
+        bgPanel.add(panelDataTransaksi);
+        bgPanel.add(labelHeaderTransaksi);
+        bgPanel.add(labelCopyright);
+
+    }
+
 }
--- src/com/view/karyawanView/transaksiView/cUpadateTransaksiView.java
+++
@@ -1,7 +0,0 @@
-package com.view.karyawanView.transaksiView;
-
-import com.main.resources.templates.cPanelContentApp;
-
-public class cUpadateTransaksiView extends cPanelContentApp {
-    
-}
