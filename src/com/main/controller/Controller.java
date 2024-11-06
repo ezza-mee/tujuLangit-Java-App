1d41fa84f49e93da62d10d225d9d493dd6d83fa5
RichardPardosi <richard12eduardo@gmail.com>
2024-11-06 23:59:50 +0700

    home-view-admin

--- src/com/main/controller/Controller.java
+++ src/com/main/controller/Controller.java
@@ -7,26 +7,26 @@ import com.view.loginView.cPanelLoginView;
 
 public class Controller {
 
-    private static cLoginView frameLogin = new cLoginView();
+    // private static cLoginView frameLogin = new cLoginView();
 
     // private static cDashboardKaryawan DashboardKaryawan = new
     // cDashboardKaryawan();
 
-    // private static cDashboardAdmin DashboardAdmin = new cDashboardAdmin();
+    private static cDashboardAdmin DashboardAdmin = new cDashboardAdmin();
 
-    public static void showLoginAdmin() {
-        frameLogin.initsLoginView();
-        frameLogin.setVisible(true);
-    }
+    // public static void showLoginAdmin() {
+    //     frameLogin.initsLoginView();
+    //     frameLogin.setVisible(true);
+    // }
 
     // public static void showDashboardKaryawan() {
     // DashboardKaryawan.initsViewDashboardKaryawan();
     // DashboardKaryawan.setVisible(true);
     // }
 
-    // public static void showDashboardAdmin() {
-    // DashboardAdmin.initsSidebarAdmin();
-    // DashboardAdmin.setVisible(true);
-    // }
+    public static void showDashboardAdmin() {
+    DashboardAdmin.initsViewDashboardAdmin();
+    DashboardAdmin.setVisible(true);
+    }
 
 }
--- src/com/view/adminView/cDashboardAdmin.java
+++ src/com/view/adminView/cDashboardAdmin.java
@@ -3,11 +3,18 @@ package com.view.adminView;
 import com.main.resources.templates.cFrameDashboardApp;
 
 public class cDashboardAdmin extends cFrameDashboardApp {
+
+    private cHomeView componentHomeView = new cHomeView();
+
     public cDashboardAdmin(){
         super();
+        initsViewDashboardAdmin();
     }
-    public void initsSidebarAdmin(){
+    public void initsViewDashboardAdmin(){
         setVisible(true);
+        componentHomeView.setBounds(240, 70, 1126, 698);
+
+        bgFrame.add(componentHomeView);
         
     }
 }
