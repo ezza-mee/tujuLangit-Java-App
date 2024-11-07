aa5f2d6f73e84ecbf07172d9c76268ac694a272f
ezza-mee <rezaaryairhamilah@gmail.com>
2024-11-08 00:30:58 +0700

    fix-marge

--- src/com/main/App.java
+++ src/com/main/App.java
@@ -4,8 +4,8 @@ import com.main.controller.*;
 
 public class App {
     public static void main(String[] args) {
-        Controller.hiddenDashboardAdmin();
-        Controller.showLoginAdmin();
+        // Controller.hiddenDashboardAdmin();
+        // Controller.showLoginAdmin();
         // Controller.showDashboardKaryawan();
         // Controller.showDashboardAdmin();
 
