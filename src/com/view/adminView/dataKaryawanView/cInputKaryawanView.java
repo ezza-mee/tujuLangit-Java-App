8784b6aa510e96c4802787498bfeec79a3be52d8
ezza-mee <rezaaryairhamilah@gmail.com>
2024-11-07 21:09:40 +0700

    add-label-input-data-karyawan

--- src/com/view/adminView/dataKaryawanView/cInputKaryawanView.java
+++ src/com/view/adminView/dataKaryawanView/cInputKaryawanView.java
@@ -11,12 +11,20 @@ public class cInputKaryawanView extends cPanelContentApp {
     private cPanelContentView parentPanel;
 
     // component label header Data Karyawan
-    private cBigFont labelHeaderDataKaryawan = new cBigFont("Product", 40, 5);
+    private cBigFont labelHeaderDataKaryawan = new cBigFont("Stafff", 40, 5);
 
     // component copyright
     private cLabelInfo labelCopyright = new cLabelInfo("CopyRight 2024. TujuLangit ForestPark", 0, 650, 1126, 40);
 
-    // component input data karyawan
+    // component panel Data karyawan 
+    private cPanelRounded panelInputKaryawan = new cPanelRounded(40, 80, 1050, 560, 10, 10);
+
+    // component label Data Karyawan 
+    private cLabelInfo labelNameKaryawan = new cLabelInfo("Name Karyawan", 180, 120, 300, 30);
+    private cLabelInfo labelAgeKaryawan = new cLabelInfo("Count Karyawan", 180, 195, 300, 30);
+    private cLabelInfo labelLocationKaryawan = new cLabelInfo("Price Karyawan", 180, 270, 300, 30);
+    private cLabelInfo labelJobdeskKaryawan = new cLabelInfo("Deskripsi Karyawan", 580, 120, 300, 30);
+    private cLabelInfo labelStatusKaryawan = new cLabelInfo("Status Product", 180, 345, 300, 30);
 
     public cInputKaryawanView(cPanelContentView parentPanel) {
         super();
