1eff9b222b2385179a074a807c82fc526b8b8b6a
ezza-mee <rezaaryairhamilah@gmail.com>
2025-01-05 23:58:11 +0700

    add-table-transaction-data-transaction-view

---
+++ src/com/main/database/transaction/cDataTransaction.java
@@ -0,0 +1,42 @@
+package com.main.database.transaction;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.swing.table.DefaultTableModel;
+
+import com.main.database.cConnectionDatabase;
+
+public class cDataTransaction {
+    public static DefaultTableModel getAllTransaction() {
+
+        String[] dataHeader = { "ID", "Number", "Customer", "Amount", "Price", "Description", "Product", "Amount",
+                "price" };
+
+        DefaultTableModel tm = new DefaultTableModel(null, dataHeader);
+        String query = "SELECT * FROM vwalltransaction";
+        try (Connection conn = cConnectionDatabase.getConnection();
+                PreparedStatement state = conn.prepareStatement(query)) {
+
+            ResultSet resultData = state.executeQuery(query);
+
+            while (resultData.next()) {
+                Object[] rowData = { "T00" + resultData.getInt("idTransaction"),
+                        resultData.getInt("numberSeats"),
+                        resultData.getString("nameCustomer"),
+                        resultData.getInt("amountTransaction"),
+                        "Rp. " + resultData.getInt("priceTransaction"),
+                        resultData.getString("description"),
+                        resultData.getString("nameProduct"),
+                        resultData.getInt("amountProduct"),
+                        "Rp. " + resultData.getInt("priceProduct"),
+                };
+                tm.addRow(rowData);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return tm;
+    }
+}
--- src/com/main/database/transaction/cInsertProductTransaction.java
+++ src/com/main/database/transaction/cInsertProductTransaction.java
@@ -7,16 +7,18 @@ import java.sql.SQLException;
 import com.main.database.cConnectionDatabase;
 
 public class cInsertProductTransaction {
-    public static boolean insertProductTransaction(int idTransaction, String nameProduct) {
+    public static boolean insertProductTransaction(int idTransaction, String nameProduct, int amountProduct, int priceProduct) {
         boolean data = false;
 
-        String query = "INSERT INTO tbl_transaction_product (idTransaction, nameProduct) VALUES (?, ?)";
+        String query = "INSERT INTO tbl_transaction_product (idTransaction, nameProduct, amountProduct, priceProduct) VALUES (?, ?, ?,?)";
 
         try (Connection conn = cConnectionDatabase.getConnection();
                 PreparedStatement state = conn.prepareStatement(query)) {
 
             state.setInt(1, idTransaction); 
             state.setString(2, nameProduct); 
+            state.setInt(3, amountProduct);
+            state.setInt(4, priceProduct);
 
             if (state.executeUpdate() > 0) {
                 data = true; 
