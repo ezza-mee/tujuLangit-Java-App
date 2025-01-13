5279befd206ba43aafab8ba644d1d67dcfecb386
RichardPardosi <richard12eduardo@gmail.com>
2025-01-14 01:14:41 +0700

    add-report-staff

--- src/com/view/adminView/cSidebarAdminView.java
+++ src/com/view/adminView/cSidebarAdminView.java
@@ -182,7 +182,7 @@ public class cSidebarAdminView extends cPanelSidebarApp {
         menuReport.setBackground(cColor.WHITE);
         menuReport.setSidebarAktif();
 
-        contentView.showReportDataProductView();
+        contentView.showReportDataStaffView();
 
         setVisible(true);
     }
